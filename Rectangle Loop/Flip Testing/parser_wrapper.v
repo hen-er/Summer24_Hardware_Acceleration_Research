b8e31006d41e0ac6dff7e3d0bbb0f034c2bb9e56
Hena Ershadi <136639931+hen-er@users.noreply.github.com>
2025-07-18 11:20:22 -0400

    Update parser_wrapper.v

--- Rectangle Loop/Flip Testing/parser_wrapper.v
+++ Rectangle Loop/Flip Testing/parser_wrapper.v
@@ -1,13 +1,15 @@
+
 module parser_wrapper(
     input         clk,
     input         reset,
     input         start,
-    output [3:0] leds
+    input [15:0]  from_pc,
+    output [15:0] to_pc
 );
-    reg [7:0]  base_addr;
+    reg [15:0]  base_addr;
     reg [1:0]  r1, r2, c1, c2;
     wire [15:0] flipped_data;
-    wire done; //make output later
+    wire done; //can probably exclude this
     
     //edge-detect on start to create a one-cycle pulse
     reg start_d, start_p;
@@ -15,25 +17,28 @@ module parser_wrapper(
         if (reset) begin
             start_d <= 0;
             start_p <= 0;
+            base_addr <= 0;
+            r1        <= 0;
+            r2        <= 0;
+            c1        <= 0;
+            c2        <= 0;
         end
         else begin
             start_p <= start & ~start_d;
             start_d <= start;
-            base_addr <= 0;
-            r1        <= 1;
-            r2        <= 3;
-            c1        <= 1;
-            c2        <= 2;
+	    base_addr <= from_pc[15:8];
+            r1        <= from_pc[7:6];
+            r2        <= from_pc[5:4];
+            c1        <= from_pc[3:2];
+            c2        <= from_pc[1:0];
         end
     end
-    
-    assign leds = flipped_data[3:0];
 
     flip_controller #(
-			  .ROWS (4),
-			  .COLS (4),
-			  .DATA_WIDTH (8)
-			  ) controller_inst (
+	.ROWS (4),
+	.COLS (4),
+	.DATA_WIDTH (8)
+	 ) controller_inst (
         .clk(clk),
         .reset(reset),
         .start(start_p),
@@ -45,5 +50,6 @@ module parser_wrapper(
         .done(done),
         .flipped_out(flipped_data)
     );
-
+    
+    assign to_pc = flipped_data;
 endmodule
